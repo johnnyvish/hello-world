e7d0c9f7834dedb4b3e2233a714b84766ec88047
johnnyvish <johnny.vishnevskiy@gmail.com>
2023-08-12 23:49:00 -0400

    update

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 from potassium import Potassium, Request, Response
-
-from transformers import pipeline
-import torch
+from audiocraft.models import AudioGen
+from audiocraft.data.audio import audio_write
+import torchaudio
 
 app = Potassium("my_app")
 
-# @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-   
+    # Initialize the AudioGen model
+    audio_model = AudioGen.get_pretrained('facebook/audiogen-medium')
+    audio_model.set_generation_params(duration=5)  # generate 5 seconds of audio.
+
     context = {
-        "model": model
+        "audio_model": audio_model,
+        "hello": "world"
     }
 
     return context
 
-# @app.handler runs for every call
-@app.handler("/")
+@app.handler()
 def handler(context: dict, request: Request) -> Response:
-    prompt = request.json.get("prompt")
-    model = context.get("model")
-    outputs = model(prompt)
+    # Retrieve the audio model from the app context
+    audio_model = context.get("audio_model")
 
+    # Obtain descriptions from the request (ensure your client sends descriptions as a list)
+    descriptions = request.json.get("descriptions", [])
+
+    # Generate audio based on descriptions
+    wav = audio_model.generate(descriptions)
+
+    # Save generated audio files and collect filenames
+    filenames = []
+    for idx, one_wav in enumerate(wav):
+        filename = f'{idx}.wav'
+        audio_write(filename, one_wav.cpu(), audio_model.sample_rate, strategy="loudness", loudness_compressor=True)
+        filenames.append(filename)
+    
+    # Return the filenames in the response (modify as per your requirements)
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json = {"audio_files": filenames}, 
         status=200
     )
 
